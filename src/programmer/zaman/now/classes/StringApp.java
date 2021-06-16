4f2e15c2e85b8ac4fcb69bcdca4d95f388612983
Andres Nodas <nodasiniesta@gmail.com>
2021-06-16 21:37:13 +0700

    First Commit

---
+++ src/programmer/zaman/now/classes/StringApp.java
@@ -0,0 +1,12 @@
+package programmer.zaman.now.classes;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		String name = "Andres Nodas";
+		String nameLowerCase = name.toLowerCase();
+		
+	}
+	
+}
